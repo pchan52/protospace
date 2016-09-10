ea1f38933f0587669db1e2cfc0b14f24835801f9
池田光 <Hikaru@pchan.local>
2016-09-11 08:50:02 +0900

    add params_permit(:member) at application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :works, :profile, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :works, :profile, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :works, :profile, :avatar,:member])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :works, :profile, :avatar,:member])
   end
 
   # sign_outしたあとsign_in画面にリダイレクト
