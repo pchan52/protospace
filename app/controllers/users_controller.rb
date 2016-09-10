3c76b0e38902ff78773d9272cf6ee0679d8d38a9
池田光 <Hikaru@pchan.local>
2016-09-11 08:51:11 +0900

    edit user_controller 1.redirect if not login 2.add edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :emal, :password, :works, :profile, :avatar, :member)
   end
 end
