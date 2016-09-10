3a928c41d29954f87fd9432f2abd9fbd7fff4721
池田光 <Hikaru@pchan.local>
2016-09-10 20:19:42 +0900

    finish sign_up/sign_in/sign_out

--- db/migrate/20160907110901_devise_create_users.rb
+++ db/migrate/20160907110901_devise_create_users.rb
@@ -1,30 +1,30 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.text :works
-      t.text :profile
-      t.text :avatar
-      t.string :email
-      t.string :password
 
-    #   ## Database authenticatable
-    #   t.string :email,              null: false, default: ""
-    #   t.string :encrypted_password, null: false, default: ""
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
-    #   ## Recoverable
-    #   t.string   :reset_password_token
-    #   t.datetime :reset_password_sent_at
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-    #   ## Rememberable
-    #   t.datetime :remember_created_at
+      ## Rememberable
+      t.datetime :remember_created_at
 
-    #   ## Trackable
-    #   t.integer  :sign_in_count, default: 0, null: false
-    #   t.datetime :current_sign_in_at
-    #   t.datetime :last_sign_in_at
-    #   t.string   :current_sign_in_ip
-    #   t.string   :last_sign_in_ip
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      t.string    :name
+      t.text      :profile
+      t.string    :member
+      t.text      :works
+      t.string    :avatar
 
     #   ## Confirmable
     #   # t.string   :confirmation_token
@@ -38,13 +38,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
     #   # t.datetime :locked_at
 
 
-    #   t.timestamps null: false
-    # end
+      t.timestamps null: false
+    end
 
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # # add_index :users, :confirmation_token,   unique: true
     # # add_index :users, :unlock_token,         unique: true
-    end
   end
 end
+
