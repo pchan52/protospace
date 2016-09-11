7912facd61ab1fb5c413cf6b43f8dcf0548deb41
池田光 <Hikaru@pchan.local>
2016-09-11 12:33:30 +0900

    repair edit.html.haml and create private method at user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
+  before_action :get_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     @user.update(update_params)
+    redirect_to :root
   end
 
   private
+  def get_user
+    @user = User.find(params[:id])
+  end
+
   def update_params
     params.require(:user).permit(:name, :emal, :password, :works, :profile, :avatar, :member)
   end
