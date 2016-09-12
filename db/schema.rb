c8eb2577a8393922ac6f08eed64e6de44ea7264b
池田光 <Hikaru@pchan.local>
2016-09-13 00:20:44 +0900

    create prototype_image table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907110901) do
+ActiveRecord::Schema.define(version: 20160912141004) do
+
+  create_table "prototype_images", force: :cascade do |t|
+    t.string   "image",        limit: 255
+    t.integer  "status",       limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "name",        limit: 255
