c8c9c777d55661c13f8e1ae59a2cd79802c5b402
池田光 <Hikaru@pchan.local>
2016-09-13 22:24:17 +0900

    fix indent

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_image
-  # mount_uploader :prototype_images, ImageUploader
+  mount_uploader :prototype_images, ImageUploader
 
   accepts_nested_attributes_for :prototype_image, allow_destroy: true, reject_if: proc { |attributes| attributes['image'].blank? }
 
