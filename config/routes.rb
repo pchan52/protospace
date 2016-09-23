5fee25e0ded85ed4b5bdfe8558ea2107057dcea9
池田光 <Hikaru@pchan.local>
2016-09-23 10:47:29 +0900

    add indent before only

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes,only: [:index, :new, :show, :create]
+  resources :prototypes, only: [:index, :new, :show, :create]
   # get  '/prototypes/index'  =>    'prototypes#index'
   # get  '/prototypes/new'    =>    'prototypes#new'
   # get  '/prototypes/show'   =>    'prototypes#show'
