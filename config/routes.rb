51ba6c1963e669f14baab3bc0bc83716840c6976
池田光 <Hikaru@pchan.local>
2016-10-06 21:39:15 +0900

    delete comment-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes, only: [:index, :new, :show, :create]
-  # get  '/prototypes/index'  =>    'prototypes#index'
-  # get  '/prototypes/new'    =>    'prototypes#new'
-  # get  '/prototypes/show'   =>    'prototypes#show'
   resources :users, only: [:show, :edit, :update]
 end
