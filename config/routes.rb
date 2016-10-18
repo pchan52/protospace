7818a072342901ba345fb08cd62db5201bae8b15
pchan52 <pchan52611625@gmail.com>
2016-10-18 12:52:10 +0900

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, except: :destroy
+  resources :prototypes
   resources :users, only: [:show, :edit, :update]
 end
