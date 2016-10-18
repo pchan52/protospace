e6aac2dcd6432a03bc8b368a375fd37cc7954f12
pchan52 <pchan52611625@gmail.com>
2016-10-18 13:10:31 +0900

    create likes table

--- db/migrate/20161018040242_create_likes.rb
+++ db/migrate/20161018040242_create_likes.rb
@@ -1,8 +1,9 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-
-      t.timestamps null: false
+      t.integer :user_id
+      t.integer :prototype_id
+      t.timestamps
     end
   end
 end
