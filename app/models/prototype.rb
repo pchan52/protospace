58139b9c34dea62093ccfd1a6fe13c30795afdc1
pchan52 <pchan52611625@gmail.com>
2016-10-20 01:09:29 +0900

    change method name and add dependent

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,11 +1,11 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images
-  has_many :likes
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :prototype_images, allow_destroy: true, reject_if: proc { |attributes| attributes['image'].blank? }
   validates :name, :concept, :catch_copy, presence: true
 
-  def liked_user(user)
+  def liked_user?(user)
     likes.find_by(user_id: user)
   end
 end
