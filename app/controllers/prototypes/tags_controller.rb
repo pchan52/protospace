4943079f99f42c5128382f59e31f0abfa391bee9
pchan52 <pchan52611625@gmail.com>
2016-10-29 12:53:48 +0900

    edit controller

---
+++ app/controllers/prototypes/tags_controller.rb
@@ -0,0 +1,12 @@
+class Prototypes::TagsController < ApplicationController
+
+  def index
+     @tags = ActsAsTaggableOn::Tag.most_used
+  end
+
+  def show
+     @tag = ActsAsTaggableOn::Tag.find(params[:id])
+     @prototypes = Prototype.tagged_with(@tag).includes([:user, :tags])
+  end
+
+end
