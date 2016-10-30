7e322e72f9729e42d282fd08b403e2ed5125f5a9
pchan52 <pchan52611625@gmail.com>
2016-10-31 01:38:01 +0900

    Revert "delete comment out"
    This reverts commit 68be9f5350cd674f8e3e8393a98ee529de47fdf1.

--- app/uploaders/prototype_image_uploader.rb
+++ app/uploaders/prototype_image_uploader.rb
@@ -2,6 +2,8 @@ class PrototypeImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
+  # storage :file
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
