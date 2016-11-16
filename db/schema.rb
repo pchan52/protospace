57f9ca7937858d5fae549868c81749738f946d9e
pchan52 <pchan52611625@gmail.com>
2016-11-16 12:38:52 +0900

    edit files

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161025223727) do
-ActiveRecord::Schema.define(version: 20161020005732) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment",      limit: 65535
