8c2a26b4198d4fed6c15e42738b5ad66d01f0b13
pchan52 <pchan52611625@gmail.com>
2016-11-17 10:12:42 +0900

    controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe UsersController do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
   let(:params) {{
   id: user.id,
   user: attributes_for(:user, name: 'hoge')
