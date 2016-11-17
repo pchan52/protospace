3cc1e1891cd79df2810ca29688fff2ed7041bd3b
pchan52 <pchan52611625@gmail.com>
2016-11-17 11:33:10 +0900

    good by hash rocket

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,5 +19,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.extend ControllerMacros, :type => :controller
+  config.extend ControllerMacros, type: :controller
 end
